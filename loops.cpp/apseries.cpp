b206fe61faa8a401a5074c22883d1348608e4f80
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-04-10 20:23:29 +0200

    love coding

---
+++ loops.cpp/apseries.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+// ap series
+
+// a+n-1*d
+// a=first term
+// n=nth termd=common difrrence
+
+int ft,n,d;
+int ans;
+cout<<"enter your fist term and n th term,difrence";
+cin>>ft>>n>>d;
+for(int i=ft;i<=n;++i)
+{
+  ans=ft+i*d;
+cout<<ans;
+
+}
+}
--- loops.cpp/fabonaccifor.cpp
+++ loops.cpp/fabonaccifor.cpp
@@ -1,18 +1,22 @@
-//program for fabonacci series another method?
- #include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-  int n,first=0,second=1,next;
+  int n,next,first=0,second=1;
   cout<<"Enter a number :";
   cin>>n;
-  cout<<first<<"\t"<<second<<"\t";
-  for(int i=1;i<=n-2;i++){
+  for(int i=1;i<=n;i++){
+  if(i==1){
+  cout<<first;
+  }
+  if(i==2){
+  cout<<second;
+  }
   next=first+second;
   first=second;
   second=next;
   cout<<next<<"\t";
-
+  
   }
   return 0;
 }
