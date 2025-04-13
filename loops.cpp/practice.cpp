8a24f0dc14c7e1a8e5bab7c2d4313977f8a44731
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-04-13 20:14:08 +0200

    c

--- loops.cpp/practice.cpp
+++ loops.cpp/practice.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int main()
 {
-
+ int n,min=9;
  cout<<"Enter number :";
  cin>>n;
- 
- 
+ while(n>0){
+ int digit=n%10;
+ if(digit<min){
+     min=digit;
+ }
+ n=n/10;
+}
+cout<<"Min no is = "<<min;
 }
