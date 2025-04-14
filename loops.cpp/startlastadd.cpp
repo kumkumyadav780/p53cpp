4c4c0c0d8991c4dd9b56bbb0df4968661b2d1119
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-04-14 20:57:00 +0200

    multitable

---
+++ loops.cpp/startlastadd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n,first,last,temp;
+ cout<<"Enter number :";
+ cin>>n;
+ temp=n;
+ last=n%10;
+ while(temp>=10){
+ temp=temp/10;
+ first=temp;
+ }
+ cout<<first+last;
+}
