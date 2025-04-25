08d4dbf38702e388c5ac922246916c7cffe84a02
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-04-25 10:45:15 +0200

    pattern

--- loops.cpp/p2.cpp
+++ loops.cpp/p2.cpp
@@ -2,21 +2,12 @@
 using namespace std;
 int main()
 {
- int sal,hra,da,total;
- cout<<"Enter your salary :";
- cin>>sal;
- if(sal<=10000){
- cout<<"DA is 80% "<<(sal*80/100);
- cout<<"HRA is 20% "<<(sal*20/100);
-}
-else if(sal>10000 && sal<=20000){
- cout<<"DA is 90% "<<(sal*90/100);
- cout<<"HRA is 25% "<<(sal*25/100);
-}
-else if(sal<=20000){
- cout<<"DA is 95% "<<(sal*90/100);
- cout<<"HRA is 30% "<<(sal*30/100);
-}
-total=sal+hra+da;
-cout<<"total salary is :"<<total;
+
+ for(int i=1;i<=5;i++){
+  for(int k=5-i;k>0;k--)
+  cout<<" ";
+  for(int j=1;j<=5;j++)
+  cout<<"*";
+  cout<<endl;
+ }
 }
