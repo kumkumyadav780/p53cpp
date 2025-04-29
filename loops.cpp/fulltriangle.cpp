6817a34971d145cf5619ff0e0f9f1086f54f1b7b
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-04-29 15:49:57 +0530

    code of cpp

--- loops.cpp/fulltriangle.cpp
+++ loops.cpp/fulltriangle.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 int main()
 {
- for(int i=1;i<=5;i++){
-  for(int j=5;j>=1;j--){
- if(i>=j){
-  cout<<" *";
+//  for(int i=1;i<=5;i++){
+//   for(int j=5;j>=1;j--){
+//  if(i>=j){
+//   cout<<" *";
+//  }
+//  else{
+//  cout<<" ";
+//  }
+//   }
+//   cout<<endl;
+//  }
+//factors?
+int n;
+cout<<"Enter number :";
+cin>>n;
+for(int i=1;i<=n;i++){
+ if(n%i==0){
+                       cout<<"fact ="<<i;
  }
- else{
- cout<<" ";
- }
-  }
-  cout<<endl;
- }
+}
+
 }
