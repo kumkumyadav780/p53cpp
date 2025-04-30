637f72f4554c842c127cc9ed9df8b7c5e6caa471
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-04-30 23:01:57 +0530

    practice time

--- for.cpp
+++ for.cpp
@@ -2,16 +2,46 @@
 using namespace std;
 int main()
 {
-  int n,sum=0;
-  cout<<"Enter number :";
-  cin>>n;
-  while(n>0)
-  {
-    int digit =n%10;
-    sum= sum+digit;
-    n=n/10;
-  }
-  cout<<sum;
+//   int n,sum=0;
+//   cout<<"Enter number :";
+//   cin>>n;
+//   while(n>0)
+//   {
+//     int digit =n%10;
+//     sum= sum+digit;
+//     n=n/10;
+//   }
+//   cout<<sum;
 
-return 0;
+// return 0;
+// }
+//sum of odd numbers
+// int n=5,sum=0;
+// for(int i=1;i<=n;i++){
+//  if(i%2 !=0 ){
+//  sum=sum+i;
+//  }
+// }
+// cout<<sum;
+// }
+
+// int n=5,sum=0;
+// int i=1;
+// while(i<=n){
+//  if(i%2!=0){
+//  sum=sum+i;
+//  }
+//  i++;
+// }
+// cout<<sum;
+// }
+
+int n=5;
+for(int i=1;i<=n;i++){
+  int m=10;
+ for(int j=1;j<=m;j++)
+ cout<<"*";
+}
+cout<<endl;
+
 }
