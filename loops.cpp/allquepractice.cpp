7c986f529d2eeae6fc24055968e68da95dc7997c
kumkumyadav780 <Kumkumyadav8841@gmail.com>
2025-06-29 23:46:10 +0530

    code

--- loops.cpp/allquepractice.cpp
+++ loops.cpp/allquepractice.cpp
@@ -290,3 +290,24 @@
 //  cout<<rev;
 // }
 
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//  int n,rev=0;
+//  cout<<"Enter a number :";
+//  cin>>n;
+//  int ans=n;
+//  while (n>0)
+//  {
+// int digit=n%10;
+//  rev=rev*10+digit;
+//  n=n/10;
+ 
+
+//  }
+//  cout<<rev;
+// }
+
+
